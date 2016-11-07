75cbaef2bc1041562757b211ec2efdba0713becc
fgaleazzi <fgaleazzi@users.noreply.github.com>
2016-11-07 23:36:24 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,10 +1,18 @@
 <?php
 
-echo 'Hello World!';
-
 $token = getenv('TOKEN');
-echo "\n TOKEN = ".$token;
+$url = "https://api.telegram.org/bot".$token;
 
-echo 'Prova pull!';
+$update = file_get_contents("php://input");
+
+$updateArray = json_decode($update, TRUE);
+
+$arrayContent = print_r($updateArray, true);
+
+$chat_id = $updateArray["message"]["chat"]["id"];
+
+file_get_contents($url."/sendmessage?chat_id=".$chat_id."&text=test");
+
+print_r($update);
 
 ?>
